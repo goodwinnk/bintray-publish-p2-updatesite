a27233dde167b139472f7b636e50d42a2cb5a1e7
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-04-07 14:29:40 +0300

    Put all files under /PCK_NAME/PCK_VERSION in the repository
    Now each version folder is a valid update site URL and can be used even when new version is released.
    Another advantage is that all files are under single package directory.

--- pushToBintray.sh
+++ pushToBintray.sh
@@ -9,17 +9,19 @@ PCK_NAME=$5
 PCK_VERSION=$6
 PATH_TO_REPOSITORY=$7
 
-BINTRAY_VERSION_PATH=${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}
+
+BINTRAY_UPLOAD_VERSION_PATH=${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}
+BINTRAY_VERSION_PATH=${BINTRAY_UPLOAD_VERSION_PATH}/${PCK_NAME}/${PCK_VERSION}
 
 function main() {
+  if [ ! -z "$PATH_TO_REPOSITORY" ]; then
+     cd $PATH_TO_REPOSITORY
+  fi
+  
   deploy_updatesite
 }
 
 function deploy_updatesite() {
-  if [ ! -z "$PATH_TO_REPOSITORY" ]; then
-     cd $PATH_TO_REPOSITORY
-  fi
-
   FILES=./*
   PLUGINDIR=./plugins/*
   FEATUREDIR=./features/*
@@ -31,7 +33,7 @@ function deploy_updatesite() {
     if [[ "$f" == *content.jar ]] || [[ "$f" == *artifacts.jar ]] 
     then
       echo "Uploading p2 metadata file directly to the repository"
-      curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/$f;publish=0
+      curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
     else 
       curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/$f;publish=0
     fi
@@ -48,16 +50,15 @@ function deploy_updatesite() {
   done
 
   echo "Processing plugin dir $PLUGINDIR file..."
-
   for f in $PLUGINDIR;
   do
     echo "Processing plugin: $f file..."
     curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/$f;publish=0
     echo ""
   done
-
+  
   echo "Publishing the new version"
-  curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/publish -d "{ \"discard\": \"false\" }"
+  curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
 }
 
 
