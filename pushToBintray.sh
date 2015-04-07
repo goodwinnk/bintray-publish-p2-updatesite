e7c8cef39ebf7caecc5f266ed83aeafcd6e65fde
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-04-07 14:02:06 +0300

    Clean-up

--- pushToBintray.sh
+++ pushToBintray.sh
@@ -9,67 +9,55 @@ PCK_NAME=$5
 PCK_VERSION=$6
 PATH_TO_REPOSITORY=$7
 
+BINTRAY_VERSION_PATH=${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}
+
 function main() {
-deploy_updatesite
+  deploy_updatesite
 }
 
 function deploy_updatesite() {
-echo "${BINTRAY_USER}"
-echo "${BINTRAY_API_KEY}"
-echo "${BINTRAY_OWNER}"
-echo "${BINTRAY_REPO}"
-echo "${PCK_NAME}"
-echo "${PCK_VERSION}"
-echo "${PATH_TO_REPOSITORY}"
-
-if [ ! -z "$PATH_TO_REPOSITORY" ]; then
-   cd $PATH_TO_REPOSITORY
-fi
-
-
-FILES=./*
-BINARYDIR=./binary/*
-PLUGINDIR=./plugins/*
-FEATUREDIR=./features/*
-
-for f in $FILES;
-do
-if [ ! -d $f ]; then
-  echo "Processing $f file..."
-  if [[ "$f" == *content.jar ]] || [[ "$f" == *artifacts.jar ]] 
-  then
-    echo "Uploading p2 metadata file directly to the repository"
-    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/$f;publish=0
-  else 
-    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
+  if [ ! -z "$PATH_TO_REPOSITORY" ]; then
+     cd $PATH_TO_REPOSITORY
   fi
-  echo ""
-fi
-done
 
-echo "Processing features dir $FEATUREDIR file..."
-for f in $FEATUREDIR;
-do
-  echo "Processing feature: $f file..."
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
-  echo ""
-done
+  FILES=./*
+  PLUGINDIR=./plugins/*
+  FEATUREDIR=./features/*
 
-echo "Processing plugin dir $PLUGINDIR file..."
+  for f in $FILES;
+  do
+  if [ ! -d $f ]; then
+    echo "Processing $f file..."
+    if [[ "$f" == *content.jar ]] || [[ "$f" == *artifacts.jar ]] 
+    then
+      echo "Uploading p2 metadata file directly to the repository"
+      curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/$f;publish=0
+    else 
+      curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/$f;publish=0
+    fi
+    echo ""
+  fi
+  done
 
-for f in $PLUGINDIR;
-do
-   # take action on each file. $f store current file name
-  echo "Processing plugin: $f file..."
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
-  echo ""
-done
+  echo "Processing features dir $FEATUREDIR file..."
+  for f in $FEATUREDIR;
+  do
+    echo "Processing feature: $f file..."
+    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/$f;publish=0
+    echo ""
+  done
 
-echo "Processing binary dir $BINARYDIR file..."
+  echo "Processing plugin dir $PLUGINDIR file..."
 
-echo "Publishing the new version"
-curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
+  for f in $PLUGINDIR;
+  do
+    echo "Processing plugin: $f file..."
+    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/$f;publish=0
+    echo ""
+  done
 
+  echo "Publishing the new version"
+  curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} ${API}/content/${BINTRAY_VERSION_PATH}/publish -d "{ \"discard\": \"false\" }"
 }
 
 
